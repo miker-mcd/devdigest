ded189c31593288d987efbb7302ddd97cdbfa0f4
miker-mcd <michaelrmcd@gmail.com>
2018-03-13 10:43:21 -0700

    Implement isSearched to filter list by title

--- src/App.js
+++ src/App.js
@@ -21,15 +21,19 @@ const list = [
   }
 ];
 
+const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       list,
+      searchTerm: '',
     };
 
     this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
 
   onDismiss(id) {
@@ -38,26 +42,41 @@ class App extends Component {
     this.setState({list: updatedList});
   }
 
+  onSearchChange(event) {
+    this.setState({searchTerm: event.target.value});
+  };
+
   render() {
     return (
       <div className="App">
-        {this.state.list.map(item =>
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <button
-                onClick={() => this.onDismiss(item.objectID)}
-                type='button'
-              >
-                Dismiss
-              </button>
-            </span>
-          </div>
+        <form>
+          <input type='text'
+          onChange={this.onSearchChange}
+           />
+        </form>
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item => {
+          const onHandleDismiss = () =>
+            this.onDismiss(item.objectID);
+
+          return (
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              <span>
+                <button
+                  onClick={onHandleDismiss}
+                  type='button'
+                >
+                  Dismiss
+                </button>
+              </span>
+            </div>
+          )
+        }
         )}
       </div>
     );
